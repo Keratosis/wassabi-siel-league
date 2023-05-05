0c255d139d27cba6163d63c91dce9c3aa21d826f
braxtonshaffie <braxtonshaffie@gmail.com>
2023-05-05 09:24:25 +0300

    adding table on team info

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
  import GamePreview from './components/GamePreview '
 import GamePreviewDetails from './components/GamePreviewDetails'
 import GamePreviewTable from './components/GamePreviewTable'
+import TeamInfo from './components/TeamInfo'
 
 
 
@@ -13,14 +14,14 @@ const App = () => {
 
 
   return (<>
-   <nav>
+   <nav className=' nav '>
 
-    <Link to="/">Home</Link> {""}
-    <Link to="/games">Games</Link>{" "}
-    <Link to="/preview">upcoming matchs</Link>{" "}
-   <Link to="/table">Details</Link>
+<Link className='link-4' to="/">Home</Link>   {""}
+    <Link  className='link-3' to="/games">Games</Link>{" "}
+    <Link  className='link-2' to="/preview">upcoming matchs</Link>{" "}
+   <Link  className='link-1' to="/table">Details</Link>
 
-   <Header />
+  
   </nav> 
   
 
@@ -30,7 +31,7 @@ const App = () => {
   <Route path="/" element={<Header/>} />
   <Route path="/GAMES" element={<GamePreview/>} />
   <Route path="/preview"  />
-  <Route path="/table" />
+  <Route path="/table" element={<  TeamInfo/>} />
 </Routes>
    
    
--- src/components/TeamInfo.jsx
+++ src/components/TeamInfo.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const TeamInfo = () => {
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:4002/team`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setGames(data)
+      })
+      .catch(error => console.error(error))
+  }, [])
+
   return (
-    <div>TeamInfo
-
-
-
+    <div>
+      <h3>Team Info</h3>
+      <div style={{ display: 'flex' }}>
+        <div style={{ marginRight: '20px' }}>
+          <h4>Western Conference:</h4>
+          {games.map((game, index) => (
+            <div key={index}>
+              {game["WESTERN CONFERENCE"].map((team, id) => (
+                <p key={id}>{team}</p>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div>
+          <h4>Eastern Conference:</h4>
+          {games.map((game, index) => (
+            <div key={index}>
+              {game["EASTERN CONFERENCE"].map((team, id) => (
+                <p key={id}>{team}</p>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
-export default TeamInfo;
+
+export default TeamInfo
