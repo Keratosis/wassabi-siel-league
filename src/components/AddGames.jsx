9e06cbb052b8d54715d28aa7c8743901fadad968
+ <+>
2023-05-07 22:16:34 +0300

    updated AddGame Component

--- src/components/AddGames.jsx
+++ src/components/AddGames.jsx
@@ -1,137 +1,206 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./AddGames.css";
 
-function AddGames({ onAddGame }) {
-  const [inputData, setInputData] = useState({
-    home: '',
-    away: '',
-    conference: '',
-    day: '',
-    date: '',
-    time: '',
-    court: '',
-    location: ''
+function AddGames() {
+  const [gameData, setGameData] = useState({
+    GAME: "",
+    HOME: "",
+    AWAY: "",
+    CONFERENCE: "",
+    DAY: "",
+    DATE: "",
+    TIME: "",
+    COURT: "",
+    LOCATION: "",
+    score1: "",
+    score2: "",
+    notes: "",
+    IMAGE1: "",
+    IMAGE2: ""
   });
 
-  const navigate = useNavigate();
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setGameData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newGame = {
-      HOME: inputData.home,
-      AWAY: inputData.away,
-      CONFERENCE: inputData.conference,
-      DAY: inputData.day,
-      DATE: inputData.date,
-      TIME: inputData.time,
-      COURT: inputData.court,
-      LOCATION: inputData.location,
-      score1: '',
-      score2: '',
-      notes: ''
-    };
-
-    fetch('http://localhost:4003/games', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newGame)
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("http://localhost:3000/games", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(gameData)
     })
       .then(response => response.json())
-      .then((newGame) => {
-        onAddGame(newGame);
-        alert("Data Posted Successfully");
-        navigate('/');
-      })
-      .catch(error => console.error(error));
-  };
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+  }
 
   return (
-    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
-      <div className='w-50 border bg-secondary text-white p-5'>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="home">Home:</label>
-            <input
-              type="text"
-              name="home"
-              className="form-control"
-              value={inputData.home}
-              onChange={e => setInputData({ ...inputData, home: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="away">Away:</label>
-            <input
-              type="text"
-              name="Away"
-              className="form-control"
-              value={inputData.away}
-              onChange={e => setInputData({ ...inputData, away: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="conference">Conference:</label>
-            <input
-              type="text"
-              name="conference"
-              className="form-control"
-              value={inputData.conference}
-              onChange={e => setInputData({ ...inputData, conference: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="day">Day:</label>
-            <input
-              type="text"
-              name="day"
-              className="form-control"
-              value={inputData.day}
-              onChange={e => setInputData({ ...inputData, day: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="date">Date:</label>
-            <input
-              type="text"
-              name="date"
-              className="form-control"
-              value={inputData.date}
-              onChange={e => setInputData({ ...inputData, date: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="time">Time:</label>
-            <input
-              type="text"
-              name="Time"
-              className="form-control"
-              value={inputData.time}
-              onChange={e => setInputData({ ...inputData, time: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="court">Court:</label>
-            <input
-              type="text"
-              name="court"
-              className="form-control"
-              value={inputData.court}
-              onChange={e => setInputData({ ...inputData, court: e.target.value })}
-            />
-          </div>
-          <div>
-            <label htmlFor="location">Location:</label>
-            <input
-              type="text"
-              name="location"
-              className="form-control"
-              value={inputData.location}
-              onChange={e => setInputData({ ...inputData, location: e.target.value })}
-            />
-          </div>
-          <button className="btn btn-gameinfo" type="submit">AddGame</button>
-        </form>
-      </div>
+    <div className="add-games-container">
+      <h2 className="add-games-header">Add Games</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="game">
+          Game:
+          <input
+            type="text"
+            name="GAME"
+            id="game"
+            value={gameData.GAME}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="home-team">
+          Home Team:
+          <input
+            type="text"
+            name="HOME"
+            id="home-team"
+            value={gameData.HOME}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="away-team">
+          Away Team:
+          <input
+            type="text"
+            name="AWAY"
+            id="away-team"
+            value={gameData.AWAY}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="conference">
+          CONFERENCE:
+          <input
+            type="text"
+            name="CONFERENCE"
+            id="conference"
+            value={gameData.CONFERENCE}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="day">
+          DAY:
+          <input
+            type="text"
+            name="DAY"
+            id="day"
+            value={gameData.DAY}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="date">
+          DATE:
+          <input
+            type="text"
+            name="DATE"
+            id="date"
+            value={gameData.DATE}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="time">
+          TIME:
+          <input
+            type="text"
+            name="TIME"
+            id="time"
+            value={gameData.TIME}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="court">
+          COURT:
+          <input
+            type="text"
+            name="COURT"
+            id="court"
+            value={gameData.COURT}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="location">
+          LOCATION:
+          <input
+            type="text"
+            name="LOCATION"
+            id="location"
+            value={gameData.LOCATION}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="score-1">
+          score1:
+          <input
+            type="text"
+            name="score1"
+            id="score-1"
+            value={gameData.score1}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="score-2">
+          score2:
+          <input
+            type="text"
+            name="score2"
+            id="score-2"
+            value={gameData.score2}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="notes">
+          notes:
+          <input
+            type="text"
+            name="notes"
+            id="notes"
+            value={gameData.notes}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="image-1">
+          IMAGE1:
+          <input
+            type="text"
+            name="IMAGE1"
+            id="image-1"
+            value={gameData.IMAGE1}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="image-2">
+          IMAGE2:
+          <input
+            type="text"
+            name="IMAGE2"
+            id="image-2"
+            value={gameData.IMAGE2}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit" className="add-games-btn">Add Game</button>
+      </form>
     </div>
   );
 }
